d01bde3109c5fbc711560a15e146a87adede83f6
sethiyanisal <sethiya1098@gmail.com>
2022-10-18 21:24:26 +0530

    added the model for chat

--- server/model/chatModel.js
+++ server/model/chatModel.js
@@ -20,6 +20,51 @@ const getRoomsByBuyer = (buyerID, res) => {
   });
 };
 
+const getChatsByRoomID = (roomID, res) => {
+    return new Promise((resolve, reject) => {
+      db.getConnection((err, connection) => {
+        if (err) {
+          return res.json({ error: "Internal Server Error" });
+        } else {
+          const sql = "SELECT * FROM chat_messages WHERE roomID = ?";
+          connection.query(sql, [roomID], (error, results) => {
+            connection.release();
+  
+            if (error) {
+              reject();
+            } else {
+              resolve(results);
+            }
+          });
+        }
+      });
+    });
+  };
+  
+  const addMessage = (roomID, message, sentBy, sentDate, sentTime, res) => {
+    db.getConnection((err, connection) => {
+      if (err) {
+        return res.json({ error: "Internal Server Erro" });
+      } else {
+        const sql =
+          "INSERT INTO chat_messages (roomID, message, sentBy, sentDate, sentTime) VALUES (?, ?, ?, ? ,?)";
+        connection.query(
+          sql,
+          [roomID, message, sentBy, sentDate, sentTime],
+          (err, results) => {
+            if (err) {
+              return res.json({ error: "Internal Server Error" });
+            } else {
+              res.json({ success: "success!" });
+            }
+          }
+        );
+      }
+    });
+  };
+
 module.exports ={
     getRoomsByBuyer,
+    getChatsByRoomID,
+    addMessage,
 }
