efb7443498e17f03a4a0e5e878218e6a45e7a372
Chathuri Priyangika <wscpriyangika@gmail.com>
2022-10-24 20:15:21 +0530

    issues forecast page added

---
+++ client/src/components/sys/admins/IssuesForecast.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import NavBar from "../../NavBar";
+import { useParams } from "react-router-dom";
+import axios from "../../../api/axios";
+
+const GEL_ISSUE_FORECAST_URL = "/admins/reports/issues";
+
+const IssuesForecast = () => {
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex justify-center mx-32">
+        <h1 className="text-4xl font-bold my-10 text-center">
+          Issues ForeCasts
+        </h1>
+      </div>
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full text-center">
+                <thead class="border-b bg-gray-800">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-white px-6 py-4"
+                    >
+                      #
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Date
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Ticket_id
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Issue Type
+                    </th>
+                    <th
+                      scope="col"
+                      class="text-sm font-medium text-white px-6 py-4"
+                    >
+                      Current Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {records?.map((record, index) => {
+                    return (
+                      <>
+                        <tr class="bg-white border-b" key={index}>
+                          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {index + 1}
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {record.Date}
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {record.ticket_id}
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {record.type}
+                          </td>
+                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {record.status}
+                          </td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center mx-32 my-20">
+          {button && (
+            <>
+              <button onClick={handleGenerate}>
+                <p className="px-5 py-3 bg-orange-500 text-white text-lg rounded-md">
+                  Generate PDF
+                </p>
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default IssuesForecast;
